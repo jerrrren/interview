8a3828c597f760d60296031774fffa67f592ed3f
jerrrren <cheejeren@yahoo.com.sg>
2023-02-25 10:31:59 +0800

    edit access-control-allow-origin

--- routerMiddleware/authMiddleware.go
+++ routerMiddleware/authMiddleware.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"interviewProject/auth"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,10 +28,10 @@ func Authenticate() gin.HandlerFunc {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://commit-interview.herokuapp.com")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With,token")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT,PATCH")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
